a899cb3fa6e18c70a34273e3112f0528268629a3
Shyam <67902666+shyam2511@users.noreply.github.com>
2023-04-26 14:56:57 +0530

    Added all the initial files

---
+++ content.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action == "extract_article") {
+    console.log("Something happened");
+    var header = document.querySelector("h1.ArticleHeader-headline").innerText;
+    var body = Array.from(document.querySelectorAll("div.ArticleBody-articleBody p")).map(p => p.innerText).join("\n");
+    sendResponse({header: header, body:body});
+}
+});
