aade977520810f8fa84daa042a88b8105425132e
Cowwl <84736016+Cowwl@users.noreply.github.com>
2023-04-27 00:02:54 +0530

    loading bar

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 document.getElementById("extract").addEventListener("click", function () {
+  // Show loading animation inside the button
+  document.getElementById("button-text").style.display = "none";
+  document.getElementById("loading").style.display = "inline-block";
+
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "extract_article" }, function (response) {
       console.log(response);
@@ -13,7 +17,11 @@ document.getElementById("extract").addEventListener("click", function () {
       .then(data => {
         setTimeout(function () {
           console.log(data);
+          // Hide loading animation inside the button
+          document.getElementById("loading").style.display = "none";
+          document.getElementById("button-text").style.display = "inline-block";
           // Add code here to process the data from the server
+          document.getElementById("response").textContent = JSON.stringify(data);
         }, 10000); // wait for 10 seconds
       });
     });
