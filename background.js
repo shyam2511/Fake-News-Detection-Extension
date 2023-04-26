6c8c356566edb3415619a17735118b1517203ee1
Cowwl <84736016+Cowwl@users.noreply.github.com>
2023-04-26 15:30:54 +0530

    ok

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("cnbc")) {
+  if (tab.url && tab.url.includes("cnbc")) {
 
 
-        console.log("Hello world");
-        chrome.tabs.sendMessage(tabId, {type : "NEW", value: ''},function(response) {
-        if (!chrome.runtime.lastError) {
-            console.log('response', response);
-          }else{
-            console.log(chrome.runtime.lastError);
-          }
-        }
-      
-      );
+    console.log("Hello world");
+    chrome.tabs.sendMessage(tabId, { type: "NEW", value: '' }, function (response) {
+      if (!chrome.runtime.lastError) {
+        console.log('response', response);
+      } else {
+        console.log(chrome.runtime.lastError);
+      }
     }
-  });
+
+    );
+  }
+});
--- popup.js
+++ popup.js
@@ -1,12 +1,18 @@
-document.getElementById("extract").addEventListener("click", function() {
-    console.log("Hello world");
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "extract_article"}, function(response) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "https://your-api-endpoint.com", true);
-        console.log("this works");
-        xhr.setRequestHeader("Content-Type", "application/json");
-        console.log(response);
-        xhr.send(JSON.stringify(response));
+document.getElementById("extract").addEventListener("click", function () {
+  console.log("Hello world");
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "extract_article" }, function (response) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://mycontainer.dthdcjhebrc7dzf0.southeastasia.azurecontainer.io:8000/predict", true);
+      console.log("this works");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      console.log(response);
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          console.log(xhr.responseText);
+        }
+      };
+      xhr.send(JSON.stringify(response));
+    });
+  });
 });
-});});
