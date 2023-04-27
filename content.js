baac83d627a4641447fd8674db3ccaa83e19e694
Cowwl <84736016+Cowwl@users.noreply.github.com>
2023-04-27 19:54:35 +0530

    updated functionality for all sites

--- content.js
+++ content.js
@@ -1,10 +1,68 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action == "extract_article") {
-    
-    const title = document.querySelector("h1.ArticleHeader-headline").innerText;
-    const body = Array.from(document.querySelectorAll("div.ArticleBody-articleBody p")).map(p => p.innerText).join("\n");
-    // console.log(title);
-    // console.log(body);
-    sendResponse({title: title, body:body});
-}
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "extract_article") {
+    let title = "";
+    let body = "";
+
+    // Get title 
+    title = document.querySelector("title").textContent;
+
+    // Check multiple meta tags and og tags for content 
+    getContentFromTags(["content", "og:description", "description"]);
+
+    // Check for dedicated content containers 
+    getContentFromElements(["#content", ".article-content", ".entry-content"]);
+
+    // Get content from <p> paragraphs (fallback) 
+    if (!body) {
+      body = getContentFromParagraphs();
+    }
+
+    // Cleanup and condense content 
+    body = body.trim().replace(/\s+/g, ' ');
+
+    // Send response 
+    sendResponse({ title: title, body: body });
+  }
 });
+
+function getContentFromTags(tags) {
+  for (const tag of tags) {
+    const metaTag = document.querySelector(`meta[name="${tag}"]`);
+    if (metaTag?.body) {
+      body = metaTag.body;
+      return;
+    }
+  }
+}
+
+function getContentFromElements(selectors) {
+  for (const selector of selectors) {
+    const contentEl = document.querySelector(selector);
+    if (contentEl?.textContent) {
+      body = contentEl.textContent;
+      return;
+    }
+  }
+}
+
+function getContentFromParagraphs() {
+  let body = "";
+  const paragraphs = document.querySelectorAll("p");
+  for (const paragraph of [...paragraphs]) {
+    body += paragraph.textContent + "\n";
+  }
+  return body;
+}
+
+function getInnerText(el) {
+  // Recursively get inner text of elements
+  let body = "";
+  for (const node of [...el.childNodes]) {
+    if (node.nodeType === 3) {
+      body += node.nodeValue;
+    } else if (node.nodeType === 1 && node.nodeName !== "SCRIPT" && node.nodeName !== "STYLE") {
+      body += getInnerText(node);
+    }
+  }
+  return body.trim();
+}
