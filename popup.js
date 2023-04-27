73f724f7f8697ecf001911c3fe8e2f1cb4c581de
Cowwl <84736016+Cowwl@users.noreply.github.com>
2023-04-27 21:51:57 +0530

    handles factcheck API

--- popup.js
+++ popup.js
@@ -2,12 +2,11 @@ document.getElementById("extract").addEventListener("click", function () {
   // Show loading animation inside the button
   document.getElementById("button-text").style.display = "none";
   document.getElementById("loading").style.display = "inline-block";
-
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "extract_article" }, function (response) {
-      //remove sentences with less than 50 characters from the body. Sentences are separated by \n
+      // Remove sentences with less than 50 characters from the body. Sentences are separated by \n
       response.body = response.body.split('\n').filter(sentence => sentence.length > 50)
-      //join the sentences back together
+      // Join the sentences back together
       response.body = response.body.join('. ')
       console.log(response);
       fetch('http://127.0.0.1:8000/predict', {
@@ -24,19 +23,19 @@ document.getElementById("extract").addEventListener("click", function () {
           document.getElementById("loading").style.display = "none";
           document.getElementById("button-text").style.display = "inline-block";
 
-          // Process the data from the server
-          let prediction = data.ensemble_prediction;
-          let probability = data.ensemble_probabilities[prediction];
+          // Check if the response is from Google Fact Check API
+          if (data.hasOwnProperty('textualRating')) {
+            const resultValue = document.getElementById("result-value");
+            resultValue.innerHTML = `Fact Check Result: <strong>${data.textualRating}</strong><br>Source: <a href="${data.url}" target="_blank">${data.publisher.site}</a>`;
+          } else {
+            // Process the data from the server
+            let prediction = data.ensemble_prediction;
+            let probability = data.ensemble_probabilities[prediction];
 
-          // Check if probability is less than 50%
-          // if (probability < 0.5) {
-          //   prediction = prediction === 'Real' ? 'Fake' : 'Real';
-          //   probability = 1 - probability;
-          // }
-
-          // Display the prediction result in the prediction result container
-          const resultValue = document.getElementById("result-value");
-          resultValue.textContent = `The article seems to be ${prediction.toLowerCase()}. Confidence: ${(probability * 100).toFixed(2)}%`;
+            // Display the prediction result in the prediction result container
+            const resultValue = document.getElementById("result-value");
+            resultValue.textContent = `The article seems to be ${prediction.toLowerCase()}. Confidence: ${(probability * 100).toFixed(2)}%`;
+          }
         })
         .catch(error => {
           console.error('Error:', error);
