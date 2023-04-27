663213705ac73fb0dc373814c18d0adc075e6b93
Cowwl <84736016+Cowwl@users.noreply.github.com>
2023-04-27 15:01:49 +0530

    UI working

--- popup.js
+++ popup.js
@@ -13,34 +13,36 @@ document.getElementById("extract").addEventListener("click", function () {
           'Content-type': 'application/json; charset=UTF-8'
         }
       })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          // Hide loading animation inside the button
-          document.getElementById("loading").style.display = "none";
-          document.getElementById("button-text").style.display = "inline-block";
-          // Add code here to process the data from the server
-          const prediction = data.ensemble_prediction;
-          const probability = data.ensemble_probabilities[prediction];
-          // Display the prediction result in the prediction result container
-          // Hide the rest of the extension
-          // document.querySelector(".card-text").style.display = "none";
-          const predictionResult = document.getElementById("prediction-result");
-          const resultText = document.getElementById("result-text");
-          const resultValue = document.getElementById("result-value");
-          resultValue.textContent = `${prediction} (${(probability * 100).toFixed(2)}%)`;
-          resultText.style.display = "block";
-          resultValue.style.display = "block";
-          predictionResult.style.display = "block";
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          // Hide loading animation inside the button
-          document.getElementById("loading").style.display = "none";
-          document.getElementById("button-text").style.display = "inline-block";
-          // Add code here to handle errors
-          alert("An error occurred. Please try again later.");
-        });
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Hide loading animation inside the button
+        document.getElementById("loading").style.display = "none";
+        document.getElementById("button-text").style.display = "inline-block";
+
+        // Process the data from the server
+        let prediction = data.ensemble_prediction;
+        let probability = data.ensemble_probabilities[prediction];
+
+        // Check if probability is less than 50%
+        if (probability < 0.5) {
+          prediction = prediction === 'Real' ? 'Fake' : 'Real';
+          probability = 1 - probability;
+        }
+
+        // Display the prediction result in the prediction result container
+        const resultValue = document.getElementById("result-value");
+        resultValue.textContent = `The article seems to be ${prediction.toLowerCase()}. Confidence: ${(probability * 100).toFixed(2)}%`;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Hide loading animation inside the button
+        document.getElementById("loading").style.display = "none";
+        document.getElementById("button-text").style.display = "inline-block";
+
+        // Handle errors
+        alert("An error occurred. Please try again later.");
+      });
     });
   });
 });
